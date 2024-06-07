8545352a0ca78425772499879693c9a17e9d0121
Adrian Dzida <adriandzida1@gmail.com>
2024-06-07 17:09:22 +0200

    fix for test deploy

--- my-books-api/src/app.ts
+++ my-books-api/src/app.ts
@@ -1,21 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import booksRoutes from './routes/booksRoutes';
-import authRoutes from './routes/authRoutes';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import booksRoutes from "./routes/booksRoutes";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: process.env.ALLOWED_ORIGIN, 
-  optionsSuccessStatus: 200, 
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
-app.use('/api', booksRoutes);
-app.use('/api', authRoutes);
+app.use("/api", booksRoutes);
+app.use("/api", authRoutes);
 
 export default app;
